e1191dac80a2a38cad5f91b32333f00ddf69bc6b
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-02 23:04:41 +0530

    feat: add seeders

---
+++ seeders/20220302085249-todo-1.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('todos', [{
+      title: 'Get things',
+      description: 'Get stationary things from store',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('todos', null, {});
+  },
+};
