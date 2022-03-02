588b4a403360c8a29948b7c8ce8234f8c6596be7
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-02 12:38:58 +0530

    feat: add migrations

---
+++ migrations/20220302065821-update-todo.js
@@ -0,0 +1,20 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.addColumn('todos', 'description', Sequelize.STRING);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
